23e54f3f609687a3eb85a6b4f8a77e53423b7694
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2026-07-24 16:38:37 +0530

    adding position embeddings and vector embedding

--- src/input_embedding.py
+++ src/input_embedding.py
@@ -1,6 +1,11 @@
 import torch
 from torch import nn
 
+from main import position_ids
+from src import position_embedding
+from src.embedding import MyEmbedding
+from src.position_embedding import PositionEmbedding
+
 
 class InputEmbedding(nn.Module):
     def __init__(
@@ -10,3 +15,28 @@ class InputEmbedding(nn.Module):
         embedding_dim: int,
     ):
         super().__init__()
+
+        self.token_embedding = MyEmbedding(
+            vocab_size=vocab_size,
+            embedding_dim=embedding_dim,
+        )
+
+        self.position_embedding = PositionEmbedding(
+            max_sequence_length=max_sequence_length,
+            embedding_dim=embedding_dim,
+        )
+
+    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+        B, T = token_ids.shape
+
+        position_ids = torch.arange(
+            T,
+            device=token_ids.device,
+        )
+
+        token_embeddings = self.token_embedding(token_ids)
+        position_embeddings = self.position_embedding(position_ids)
+
+        input_embeddings = token_embeddings + position_embeddings
+
+        return input_embeddings
