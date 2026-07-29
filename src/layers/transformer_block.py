1b07ab26b58024deab540d4900423b14858b736f
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2026-07-29 19:57:55 +0530

    Refactoring

--- src/layers/transformer_block.py
+++ src/layers/transformer_block.py
@@ -15,11 +15,11 @@ class TransformerBlock(nn.Module):
     ) -> None:
         super().__init__()
 
-        self.embedding_dim: int = embedding_dim
-        self.num_heads: int = num_heads
-        self.max_sequence_length: int = max_sequence_length
+        # self.embedding_dim: int = embedding_dim
+        # self.num_heads: int = num_heads
+        # self.max_sequence_length: int = max_sequence_length
 
-        self.attention_layer_norm = nn.LayerNorm(normalized_shape=embedding_dim)
+        self.attention_norm = nn.LayerNorm(normalized_shape=embedding_dim)
 
         self.multi_head_attention = MultiHeadAttention(
             embedding_dim=embedding_dim,
@@ -27,24 +27,40 @@ class TransformerBlock(nn.Module):
             max_sequence_length=max_sequence_length,
         )
 
-        self.feed_forward_layer_norm = nn.LayerNorm(normalized_shape=embedding_dim)
+        self.feed_forward_norm = nn.LayerNorm(normalized_shape=embedding_dim)
 
         self.feed_forward = FeedForward(embedding_dim=embedding_dim)
 
     def forward(
         self,
-        x: torch.Tensor,
+        hidden_states: torch.Tensor,
     ) -> torch.Tensor:
-        normalized_x = self.attention_layer_norm(x)
+        # normalized_x = self.attention_norm(x)
 
-        attention_output = self.multi_head_attention(normalized_x)
+        # attention_output = self.multi_head_attention(normalized_x)
 
-        x = x + attention_output
+        # x = x + attention_output
 
-        normalized_x = self.feed_forward_layer_norm(x)
+        # normalized_x = self.feed_forward_norm(x)
 
-        feed_forward_output = self.feed_forward(normalized_x)
+        # feed_forward_output = self.feed_forward(normalized_x)
 
-        x = x + feed_forward_output
+        # x = x + feed_forward_output
 
-        return x
+        # return x
+
+        residual = hidden_states
+
+        hidden_states = self.attention_norm(hidden_states)
+        hidden_states = self.multi_head_attention(hidden_states)
+
+        hidden_states = residual + hidden_states
+
+        residual = hidden_states
+
+        hidden_states = self.feed_forward_norm(hidden_states)
+        hidden_states = self.feed_forward(hidden_states)
+
+        hidden_states = residual + hidden_states
+
+        return hidden_states
