8f4ca995431dec9b136ce62976d02953db35fc7c
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2026-07-30 16:51:29 +0530

    added gpt-config-dataclass

---
+++ src/configs/gpt_cnfig.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass(slots=True)
+class GPTConfig:
+    vocab_size: int
+    max_sequence_length: int
+
+    embedding_dim: int
+    num_heads: int
+    num_layer: int
+
+    dropout_probability: float = 0.1
+
+    bias: bool = False
