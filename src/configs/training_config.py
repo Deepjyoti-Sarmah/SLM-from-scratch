05d03d5a2d16d060bdcb4f590a91f0b1d428549a
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2026-08-03 16:44:58 +0530

    added trainer file

---
+++ src/configs/training_config.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+
+@dataclass(slots=True)
+class TrainingConfig:
+    batch_size: int = 64
+
+    learning_rate: float = 3e-4
+
+    weight_decay: float = 0.1
+
+    beta1: float = 0.9
+
+    beta2: float = 0.95
+
+    gradient_clip: float = 1.0
+
+    num_epochs: int = 10
+
+    log_every: int = 100
+
+    checkpoint_every: int = 1000
---
+++ src/training/trainer.py
@@ -0,0 +1,50 @@
+import torch
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader
+
+from src.configs.training_config import TrainingConfig
+from src.models.gpt import GPT
+
+
+class Trainer:
+    def __init__(
+        self,
+        *,
+        model: GPT,
+        train_dataloader: DataLoader,
+        optimizer: Optimizer,
+        config: TrainingConfig,
+    ) -> None:
+        self.model: GPT = model
+        self.train_dataloader: DataLoader = train_dataloader
+        self.optimizer: Optimizer = optimizer
+        self.config: TrainingConfig = config
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.model.to(self.device)
+
+        self.current_epoch = 0
+        self.global_step = 0
+
+    def train(self) -> None:
+        self.model.train()
+
+        for input_ids, target_ids in self.train_dataloader:
+            input_ids = input_ids.to(self.device)
+            target_ids = target_ids.to(self.device)
+
+            self.optimizer.zero_grad()
+
+            _, loss = self.model(
+                token_ids=input_ids,
+                targets=target_ids,
+            )
+
+            assert loss is not None
+
+            loss.backward()
+
+            self.optimizer.step()
+
+            self.global_step += 1
