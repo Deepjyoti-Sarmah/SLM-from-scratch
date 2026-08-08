8d2b2eeb387ed97f56e0fbf03401b0607619162f
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2026-08-08 19:16:24 +0530

    added generator file

---
+++ src/inference/generator.py
@@ -0,0 +1,46 @@
+import torch
+
+from main import token_ids
+from src.models.gpt import GPT
+from src.tokenization.char_tokenizer import CharacterTokenizer
+
+
+class TextGenerator:
+    def __init__(
+        self,
+        *,
+        model: GPT,
+        tokenizer: CharacterTokenizer,
+        device: torch.device,
+    ) -> None:
+        self.model = model
+        self.tokenizer = tokenizer
+        self.device = device
+
+    @torch.no_grad()
+    def generate(
+        self,
+        *,
+        prompt: str,
+        max_new_token: int = 200,
+    ) -> str:
+        self.model.eval()
+
+        token_ids = self.tokenizer.encode(prompt)
+
+        token_ids_tensor = torch.tensor(
+            token_ids,
+            dtype=torch.long,
+            device=self.device,
+        ).unsqueeze(0)
+
+        generated = self.model.generate(
+            token_ids=token_ids_tensor,
+            max_new_tokens=max_new_token,
+        )
+
+        text = self.tokenizer.decode(generated[0].tolist())
+
+        self.model.train()
+
+        return text
